1a1ad0ea5a9b498452a5467a4a6db4f438404476
groovesmith <jglad16@student.aau.dk>
2019-10-16 12:18:44 +0200

    Main Repo Remade

---
+++ Build/controllers/alibot/CMakeFiles/alibot.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "alibot_autogen"
+  "CMakeFiles/alibot_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/alibot.dir/alibot.cpp.o"
+  "CMakeFiles/alibot.dir/alibot_autogen/mocs_compilation.cpp.o"
+  "libalibot.pdb"
+  "libalibot.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/alibot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Build/controllers/alibot/CMakeFiles/alibot_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "alibot_autogen"
+  "CMakeFiles/alibot_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/alibot_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/alibot_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
